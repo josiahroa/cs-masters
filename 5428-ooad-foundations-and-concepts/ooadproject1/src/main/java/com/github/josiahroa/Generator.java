020d183eff76b8cd181a79fec0c10decd679633b
Josiah Roa <siahroa25@gmail.com>
2025-08-28 22:25:59 -0600

    WIP ooad project 1

---
+++ 5428-ooad-foundations-and-concepts/ooadproject1/src/main/java/com/github/josiahroa/Generator.java
@@ -0,0 +1,171 @@
+package com.github.josiahroa;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.ArrayList;
+import java.text.DecimalFormat;
+
+/*
+ * Josiah Roa
+ * CSCA 5428 OOAD Foundations and Concepts Project 1
+ * 8/28/2025
+ */
+public class Generator {
+    // Define the width of the columns in the output for consistency in the display output
+    private static final int COL_WIDTH = 10;
+    
+    public static void main(String[] args) {
+        Generator g = new Generator();
+        g.execute();
+    }
+
+    // Provides a common interface for all random number generators to implement
+    interface RandomNumberGenerator {
+        public double generateRandomNumber();
+    }
+
+    // Random number generator that uses the Java.util.Random class
+    class RandRandomNumberGenerator implements RandomNumberGenerator {
+        public double generateRandomNumber() {
+            return new Random().nextDouble();
+        }
+    }
+
+    // Random number generator that uses the Math.random() method
+    class MathRandomNumberGenerator implements RandomNumberGenerator {
+        public double generateRandomNumber() {
+            return Math.random();
+        }
+    }
+
+    // Random number generator that uses the ThreadLocalRandom class
+    class ThreadLocalRandomRandomNumberGenerator implements RandomNumberGenerator {
+        public double generateRandomNumber() {
+            return ThreadLocalRandom.current().nextDouble();
+        }
+    }
+
+    // Factory class for creating random number generators based on the given generator id
+    class RandomNumberGeneratorFactory {
+        public RandomNumberGenerator getRandomNumberGenerator(int randNumGen) {
+            if (randNumGen == 0) return new RandRandomNumberGenerator();
+            if (randNumGen == 1) return new MathRandomNumberGenerator();
+            if (randNumGen == 2) return new ThreadLocalRandomRandomNumberGenerator();
+            return null;
+        }
+    }
+
+    /**
+     * Creates an array list of n random numbers using a specified random number generator
+     *
+     * @param n the number of random numbers to generate
+     * @param randNumGen the random number generator to use
+     * @return an array list of n random numbers
+     */
+    ArrayList<Double> populate(int n, int randNumGen) {
+        ArrayList<Double> randomValues = new ArrayList<>();
+        RandomNumberGeneratorFactory factory = new RandomNumberGeneratorFactory();
+        RandomNumberGenerator generator = factory.getRandomNumberGenerator(randNumGen);
+
+        for (int i = 0; i < n; i++) {
+            double rv = generator.generateRandomNumber();
+            randomValues.add(rv);
+        }
+
+        return randomValues;
+    }
+
+    private double mean(ArrayList<Double> randomValues) {
+        double sum = 0;
+        for (double value : randomValues) {
+            sum += value;
+        }
+        return sum / randomValues.size();
+    }
+
+    private double stddev(ArrayList<Double> randomValues) {
+        double mean = mean(randomValues);
+        double sum = 0;
+        for (double value : randomValues) {
+            sum += Math.pow(value - mean, 2);
+        }
+        return Math.sqrt(sum / randomValues.size());
+    }
+
+    private double min(ArrayList<Double> randomValues) {
+        double min = randomValues.get(0);
+        for (double value : randomValues) {
+            if (value < min) {
+                min = value;
+            }
+        }
+        return min;
+    }
+
+    private double max(ArrayList<Double> randomValues) {
+        double max = randomValues.get(0);
+        for (double value : randomValues) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        return max;
+    }
+ 
+    /**
+     * Calculates mean, sample standard deviation, minimum, and maximum
+     * from the randomValues provided
+     *
+     * @param randomValues
+     * @return an ArrayList with five results [n, mean, stddev, min, max]
+     */
+    ArrayList<Double> statistics(ArrayList<Double> randomValues) {
+        ArrayList<Double> results = new ArrayList<>();
+        results.add(0, (double)randomValues.size());
+        results.add(1, mean(randomValues));
+        results.add(2, stddev(randomValues));
+        results.add(3, min(randomValues));
+        results.add(4, max(randomValues));
+        return results; 
+    }
+
+    /**
+     * Displays the results in the format from the project description
+     * 
+     * @param results
+     * @param headerOn
+     */
+    void display(ArrayList<Double> results, boolean headerOn) {
+        // Utilize DecimalFormat to reduce the number of decimal places to 4 to keep the output clean
+        DecimalFormat df = new DecimalFormat("#.####");
+
+        double n = results.get(0);
+        String mean = df.format(results.get(1));
+        String stddev = df.format(results.get(2));
+        String min = df.format(results.get(3));
+        String max = df.format(results.get(4));
+        System.out.printf("%-" + COL_WIDTH + "s %-" + COL_WIDTH + "s %-" + COL_WIDTH + "s %-" + COL_WIDTH + "s %-" + COL_WIDTH + "s\n", n, mean, stddev, min, max);
+    }
+
+    // calls populate, statistics, and display methods for each combination of n and
+    // randomNumberGenerator â€“ a total of 9 results
+    void execute() {
+        String[] rm = { "Java.util.Random", "Math.random", "ThreadLocalRandom" };
+        // select the random number generator
+        for (int k = 0; k < 3; k++) {
+            // select n
+            for (int i = 1; i < 4; i++) {
+                int n = (int) Math.pow(10.0, (double) i);
+                ArrayList<Double> rv = populate(n, k);    // calls your populate method
+                ArrayList<Double> res = statistics(rv);   // calls your statistics method
+                boolean headerOn = false;
+                if (i == 1) {
+                    headerOn = true;
+                    System.out.println("From " + rm[k]);
+                    System.out.printf("%-" + COL_WIDTH + "s %-" + COL_WIDTH + "s %-" + COL_WIDTH + "s %-" + COL_WIDTH + "s %-" + COL_WIDTH + "s\n", "n", "mean", "stddev", "min", "max");
+                }
+                display(res, headerOn); // calls your display method
+            }
+        }
+    }
+}
